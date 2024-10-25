cfabf89b906efb891ce53dbd650bb86e3a584d1d
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-10-25 21:56:01 +0530

    Email settings for sending email notifications

--- ECommerse/EShop/EShop/settings.py
+++ ECommerse/EShop/EShop/settings.py
@@ -129,3 +129,14 @@ MEDIA_URL='/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'   # Or your SMTP server
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'goreanuja6@gmail.com' #your email address here
+EMAIL_HOST_PASSWORD = 'zadg llcl ugwd csip'    #your passward here
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
