39f985cf9c268e3aed2b13ed55788c3569e7e63d
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-10-25 21:53:25 +0530

    path added for contact_success

--- ECommerse/EShop/shop/urls.py
+++ ECommerse/EShop/shop/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name="ShopHome"),
     path('about', views.about, name="AboutUs"), 
     path('contact', views.contact, name="ContactUs"), 
+    path('contact/success', views.contact_success, name="contact_success"),
     path('tracker', views.tracker, name="TrackingStatus"), 
     path('search', views.search, name="Search"), 
     path("products<int:myid>", views.productView, name="ProductView"), 
