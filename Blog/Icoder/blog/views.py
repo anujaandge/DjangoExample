d2d56dc3c80a201ff3ccacf6ca9eeca866164664
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-11-15 15:56:50 +0530

    views creted for added paths

--- Blog/Icoder/blog/views.py
+++ Blog/Icoder/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def blogHome(request):
+    return HttpResponse('This is home')
+
+def blogPost(request, slug): 
+    return HttpResponse(f'This is blogPost : {slug}')
--- Blog/Icoder/home/views.py
+++ Blog/Icoder/home/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("home of Home")
+
+def contact(request):
+    return HttpResponse("contact of Home")
+
+def about(request):
+    return HttpResponse("about of Home")
