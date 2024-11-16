1f9e244ba822af9c43369e5b24d1519d2927fdbd
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-11-16 15:05:21 +0530

    contact function added

--- Blog/Icoder/home/views.py
+++ Blog/Icoder/home/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return HttpResponse("home of Home")
+    return render(request, 'home/home.html')
 
 def contact(request):
-    return HttpResponse("contact of Home")
+    if request.method=="POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        content =request.POST['content']
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4:
+            messages.error(request, "Please fill the form correctly")
+        else:
+            contact=Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, "Your message has been successfully sent")
+    return render(request,'home/contact.html')
 
 def about(request):
-    return HttpResponse("about of Home")
+    return render(request,'home/about.html')
