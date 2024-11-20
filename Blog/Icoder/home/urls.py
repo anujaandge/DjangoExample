a4c96b75e5931d050bdafec652f04a575f0cb33c
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-11-20 09:01:34 +0530

    urls created for search

--- Blog/Icoder/home/urls.py
+++ Blog/Icoder/home/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.home, name='Home'),
     path('contact', views.contact, name='Contact'),
     path('about', views.about, name='About'),
-    
+    path('search', views.search, name='Search'),
+    path('signup', views.handleSignUp, name="handleSignUp"),
+    path('login', views.handleLogin, name="handleLogin"),
+    path('logout', views.handleLogout, name="handleLogout"),
     
 ]
